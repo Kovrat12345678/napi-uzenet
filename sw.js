80a3247b05f43814899a52a05676abfa0f094841
Kovrat12345678 <Kovrat12345678@users.noreply.github.com>
2026-07-26 00:27:03 +0200

    feat: replace summer event with racing event, remove mega pack boxes and accessories, improve stickers UI

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'dailybot-v57';
+const CACHE_NAME = 'dailybot-v59';
 const ASSETS = [
     './',
     './index.html',
     './manifest.json',
     './icon/app_icon_event.png',
-    './summer music/Summer Chill.mp3'
+
 ];
 
 // Install — cache fájlok
